2a58ecf242fe622707cee5c57a34b0620e501877
Umar Alfarouk <medrivia@gmail.com>
2025-07-16 00:41:01 +0700

    fix: Forgot to export WSAS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export {WS, type Options} from './ws.ts'
 export * from './wsa.ts'
+export * from './wsas.ts'
 export * from './wsz.ts'
 export * from './cookie.ts'
 export { create as zodCreate } from './zod.ts'
